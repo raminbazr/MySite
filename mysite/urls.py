803356d9a2ed8e72a3313b43195dffc1d335c4da
Ramin Bazrafshan <Ramin.Bazrafsha88n@gmail.com>
2026-09-25 12:47:36 +0330

    created wesite project

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+#from website.views import http_test,Json_test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('website.urls'))
+    #path
+    #path('http_test',http_test),
+    #path('Json_test',Json_test)
 ]
